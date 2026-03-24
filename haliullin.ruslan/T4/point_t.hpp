87fbe356c9973f2080ce3c6294791873c791276d
knopkloop <hruslan2007@gmail.com>
2026-03-24 19:09:11 +0300

    added point_t

---
+++ haliullin.ruslan/T4/point_t.hpp
@@ -0,0 +1,19 @@
+#ifndef POINT_T_HPP
+#define POINT_T_HPP
+
+namespace haliullin
+{
+  struct point_t
+  {
+    double x = 0.0;
+    double y = 0.0;
+
+    point_t() = default;
+
+    point_t(double xx, double yy):
+      x(xx), y(yy)
+    {}
+  };
+}
+
+#endif
