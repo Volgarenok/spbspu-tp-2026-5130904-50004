77af16fdaddd1bcc2344c44f303567371b3744a9
knopkloop <hruslan2007@gmail.com>
2026-03-24 19:18:36 +0300

    added rectangle_t

---
+++ haliullin.ruslan/T4/rectangle_t.hpp
@@ -0,0 +1,22 @@
+#ifndef RECTANGLE_T_HPP
+#define RECTANGLE_T_HPP
+
+#include "point_t.hpp"
+
+namespace haliullin
+{
+  struct rectangle_t
+  {
+    double width = 0.0;
+    double height = 0.0;
+    point_t pos = {0.0, 0.0};
+
+    rectangle_t() = default;
+
+    rectangle_t(double w, double h, const point_t & p):
+      width(w), height(h), pos(p)
+    {}
+  };
+}
+
+#endif
