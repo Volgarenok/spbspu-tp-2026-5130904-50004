a7b6b0f14e8a698dc1c2e26340db68ad316f762e
knopkloop <hruslan2007@gmail.com>
2026-03-24 20:43:53 +0300

    added triangle declaration

---
+++ haliullin.ruslan/T4/triangle.hpp
@@ -0,0 +1,30 @@
+#ifndef TRIANGLE_HPP
+#define TRIANGLE_HPP
+
+#include "shape.hpp"
+
+namespace haliullin
+{
+  class Triangle : public Shape
+  {
+  public:
+    Triangle(const point_t & a, const point_t & b, const point_t & c);
+    ~Triangle() override = default;
+
+    double getArea() const override;
+    rectangle_t getFrameRect() const override;
+
+    void move(const point_t & pt) override;
+    void move(double dx, double dy) override;
+
+    void scale(double k) override;
+
+  private:
+    point_t p1;
+    point_t p2;
+    point_t p3;
+    point_t pos;
+  };
+}
+
+#endif
