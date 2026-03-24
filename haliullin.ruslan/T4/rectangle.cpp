25a7656ee8c45bc379b4dab92877c06f4a722a0f
knopkloop <hruslan2007@gmail.com>
2026-03-24 20:12:41 +0300

    added rectangle implementation (in previous commit was declaration, not implementation, my english bye-bye

---
+++ haliullin.ruslan/T4/rectangle.cpp
@@ -0,0 +1,44 @@
+#include "rectangle.hpp"
+
+namespace haliullin
+{
+  Rectangle::Rectangle(double w, double h, const point_t & p):
+    width(w), height(h), pos(p)
+  {
+    if (width <= 0 || height <= 0)
+    {
+      throw std::invalid_argument("Sizes must be positive");
+    }
+  }
+
+  double Rectangle::getArea() const
+  {
+    return width * height;
+  }
+
+  rectangle_t Rectangle::getFrameRect() const
+  {
+    return rectangle_t(width, height, pos);
+  }
+
+  void Rectangle::move(const point_t & pt)
+  {
+    pos = pt;
+  }
+
+  void Rectangle::move(double dx, double dy)
+  {
+    pos.x += dx;
+    pos.y += dy;
+  }
+
+  void Rectangle::scale(double k)
+  {
+    if (k <= 0)
+    {
+      throw std::invalid_argument("scale coefficient must be positive");
+    }
+    width *= k;
+    height *= k;
+  }
+}
