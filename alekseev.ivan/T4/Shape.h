c3b3944998904809797be4012f32b4a60ca57414
alekseev-ii <alekseev.ii@edu.spbstu.ru>
2026-03-27 10:31:01 +0300

    feat(shape): add iShape

--- alekseev.ivan/T4/Shape.h
+++ alekseev.ivan/T4/Shape.h
@@ -10,4 +10,14 @@ struct rectangle_t {
   point_t pos;
 };
 
+struct iShape {
+  virtual ~iShape() = default;
+
+  virtual double getArea() const = 0;
+  virtual rectangle_t getFrameRect() const = 0;
+  virtual void move(point_t new_center) = 0;
+  virtual void move(double x, double y) = 0;
+  virtual void scale(double k) = 0;
+};
+
 #endif
