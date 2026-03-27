55ce93f2ef493f426d59678a8f534c52c7967ccb
alekseev-ii <alekseev.ii@edu.spbstu.ru>
2026-03-27 10:52:32 +0300

    feat(shape): add namespace

--- alekseev.ivan/T4/Shape.cpp
+++ alekseev.ivan/T4/Shape.cpp
@@ -1,32 +1,32 @@
 #include "Shape.h"
 
-Rectangle::Rectangle(double width, double height, point_t pos):
+alekseev::Rectangle::Rectangle(double width, double height, point_t pos):
   rect_(rectangle_t{width, height, pos})
 {
 }
 
-double Rectangle::getArea() const
+double alekseev::Rectangle::getArea() const
 {
   return rect_.height_ * rect_.width_;
 }
 
-rectangle_t Rectangle::getFrameRect() const
+alekseev::rectangle_t alekseev::Rectangle::getFrameRect() const
 {
   return rect_;
 }
 
-void Rectangle::move(point_t new_center)
+void alekseev::Rectangle::move(point_t new_center)
 {
   rect_.pos_ = new_center;
 }
 
-void Rectangle::move(double x, double y)
+void alekseev::Rectangle::move(double x, double y)
 {
   rect_.pos_.x_ += x;
   rect_.pos_.y_ += y;
 }
 
-void Rectangle::scale(double k)
+void alekseev::Rectangle::scale(double k)
 {
   rect_.width_ *= k;
   rect_.height_ *= k;
