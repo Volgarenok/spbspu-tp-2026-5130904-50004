83fc9a4d8e474a606a865a97948f2caf8470a6e2
alekseev-ii <alekseev.ii@edu.spbstu.ru>
2026-03-27 10:55:30 +0300

    feat(shape): add Circle

--- alekseev.ivan/T4/Shape.cpp
+++ alekseev.ivan/T4/Shape.cpp
@@ -31,3 +31,35 @@ void alekseev::Rectangle::scale(double k)
   rect_.width_ *= k;
   rect_.height_ *= k;
 }
+
+alekseev::Circle::Circle(double radius, point_t center):
+  radius_(radius),
+  center_(center)
+{
+}
+
+double alekseev::Circle::getArea() const
+{
+  return PI * radius_ * radius_;
+}
+
+alekseev::rectangle_t alekseev::Circle::getFrameRect() const
+{
+  return rectangle_t{2 * radius_, 2 * radius_, center_};
+}
+
+void alekseev::Circle::move(point_t new_center)
+{
+  center_ = new_center;
+}
+
+void alekseev::Circle::move(double x, double y)
+{
+  center_.x_ += x;
+  center_.y_ += y;
+}
+
+void alekseev::Circle::scale(double k)
+{
+  radius_ *= k;
+}
