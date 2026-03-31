78b5fb4c17659f75a0665600b17176663fab5360
FShaaM <zinovevas135@gmail.com>
2026-03-31 20:58:09 +0300

    Added Circle methods

---
+++ zinoviev.alexander/T4/circle.cpp
@@ -0,0 +1,33 @@
+#include "circle.hpp"
+#include <cmath>
+
+double zinoviev::Circle::getArea() const
+{
+	const double PI = std::acos(-1.0);
+
+	return PI * radius_ * radius_;
+}
+
+zinoviev::rectangle_t zinoviev::Circle::getFrameRect() const
+{
+	return rectangle_t(center_, 2 * radius_, 2 * radius_);
+}
+
+void zinoviev::Circle::move(const point_t& newPos)
+{
+	center_ = newPos;
+}
+
+void zinoviev::Circle::move(double dx, double dy)
+{
+	center_.x += dx;
+	center_.y += dy;
+}
+
+void zinoviev::Circle::scale(double k)
+{
+	if (k <= 0.0)
+		throw std::logic_error("Coefficient must be positive");
+
+	radius_ *= k;
+}
