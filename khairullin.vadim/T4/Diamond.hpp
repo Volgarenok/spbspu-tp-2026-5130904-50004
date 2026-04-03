57f6835b49480734d3cf4d817652c13eb3a01095
BinaryKenny <khairullinvadim2006@gmail.com>
2026-04-03 18:24:18 +0300

    Diamond.hpp was added as a class

---
+++ khairullin.vadim/T4/Diamond.hpp
@@ -0,0 +1,16 @@
+#define DIAMOND_HPP
+#ifndef DIAMOND_HPP
+#include "Shape"
+
+struct Diamond: Shape
+{
+  double vertDiag, horDiag;
+  Diamond(point_t p, double a, double b);
+  virtual double getArea();
+  virtual rectangle_t getFrameRect();
+  virtual void move(point_t p);
+  virtual void scale(double k);
+  virtual ~Diamond() = default;
+};
+
+#endif
