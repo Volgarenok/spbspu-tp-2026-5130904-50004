d108db22ef2e4595b5491d2c5fd5ec5600cd1607
maxchadin <maxchadin080418@gmail.com>
2026-04-04 00:00:47 +0300

    added gerArea, getFrameRect, getCenter to triangle.cpp

--- chadin.maxim/T4/triangle.cpp
+++ chadin.maxim/T4/triangle.cpp
@@ -13,3 +13,22 @@ namespace chadin {
       throw std::invalid_argument("Invalid triangle vertices");
     }
   }
+
+  double Triangle::getArea() const
+  {
+    return 0.5 * std::abs(a_.x * (b_.y - c_.y) + b_.x * (c_.y - a_.y) + c_.x * (a_.y - b_.y));
+  }
+
+  rectangle_t Triangle::getFrameRect() const
+  {
+    const double min_x = std::min({a_.x, b_.x, c_.x});
+    const double max_x = std::max({a_.x, b_.x, c_.x});
+    const double min_y = std::min({a_.y, b_.y, c_.y});
+    const double max_y = std::max({a_.y, b_.y, c_.y});
+    return {max_x - min_x, max_y - min_y, {(min_x + max_x) / 2.0, (min_y + max_y) / 2.0}};
+  }
+
+  point_t Triangle::getCenter() const
+  {
+    return {(a_.x + b_.x + c_.x) / 3.0, (a_.y + b_.y + c_.y) / 3.0};
+  }
