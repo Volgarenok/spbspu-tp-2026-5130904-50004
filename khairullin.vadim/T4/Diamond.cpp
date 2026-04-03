4fdbe3d78cdbb0bedad2e81cfe4ad7f8156f82b0
BinaryKenny <khairullinvadim2006@gmail.com>
2026-04-03 18:28:02 +0300

    I added methods of Diamond

---
+++ khairullin.vadim/T4/Diamond.cpp
@@ -0,0 +1,29 @@
+#include "Diamond.hpp"
+
+Diamond::Diamond(point_t p, double a, double b):
+vertDiag(a),
+horDiag(b)
+{
+  pos = p;
+}
+double Diamond::getArea()
+{
+  return vertDiag * horDiag;
+}
+
+rectangle_t Diamond::getFrameRect()
+{
+  return rectangle_t(pos, horDiag, vertDiag);
+}
+
+void Diamond::move(point_t p)
+{
+  pos = p;
+}
+
+void Diamond::scale(double k)
+{
+  vertDiag *= k;
+  horDiag *= k;
+}
+
