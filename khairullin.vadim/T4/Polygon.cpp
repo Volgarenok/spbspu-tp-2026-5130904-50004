0868bce965d8871659a55adee3c321cd65347b2d
BinaryKenny <khairullinvadim2006@gmail.com>
2026-04-03 18:36:47 +0300

    I added methods of class Polygon

---
+++ khairullin.vadim/T4/Polygon.cpp
@@ -0,0 +1,79 @@
+#include "Polygon.hpp"
+
+Polygon::Polygon(std::vector<point_t> p):
+points(p)
+{
+  size_t n = p.size();
+  double x = 0;
+  double y = 0;
+  for (size_t i = 0; i < n; i++)
+  {
+    x += points[i].x;
+    y += points[i].y;
+  }
+  x /= n;
+  y /= n;
+  pos.x = x;
+  pos.y = y;
+}
+
+double Polygon::getArea()
+{
+  double area = 0;
+  for (size_t i = 0; i < n - 1; i++)
+  {
+    area += (points[i].x * points[i + 1].y - points[i + 1].x * points[i].y);
+  }
+  area += (points[n - 1].x * points[0].y - points[0].x * points[n - 1].y);
+  area = area < 0 ? area * (-0.5) : area * 0.5;
+  return area;
+}
+
+rectangle_t Polygon::getFrameRect()
+{
+  double x_min = points[0].x;
+  double x_max = x_min;
+  double y_min = points[0].y;
+  double y_max = y_min;
+  for (size_t i = 0; i < n; i++)
+  {
+    if (points[i].x < x_min)
+    {
+      x_min = points[i].x;
+    }
+    if (points[i].x > x_max)
+    {
+      x_max = points[i].x;
+    }
+    if (points[i].y < y_min)
+    {
+      y_min = points[i].y;
+    }
+    if (points[i].y > y_max)
+    {
+      y_max = points[i].y;
+    }
+  }
+  return rectangle_t(pos, x_max - x_min, y_max - y_min);
+}
+
+void Polygon::move(point_t p)
+{
+  double x = p.x - pos.x;
+  double y = p.y - pos.y;
+  for (size_t i = 0; i < n; i++)
+  {
+    points[i].x += x;
+    points[i].y += y;
+  }
+  pos = p;
+}
+
+void Polygon::scale(double k)
+{
+  for (size_t i = 0; i < n; i++)
+  {
+    points[i].x = pos.x + k * (points[i].x - pos.x);
+    points[i].y = pos.y + k * (points[i].y - pos.y);
+  }
+}
