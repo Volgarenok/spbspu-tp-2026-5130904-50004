7e024e9e196fec55080ca07cea1962a9a1b4a66b
ryouta25 <avp22.07@mail.ru>
2026-04-04 21:13:12 +0300

    Implement Triangle constructor

---
+++ pozdeev.andrey/T4/triangle.cpp
@@ -0,0 +1,23 @@
+#include "triangle.hpp"
+
+namespace pozdeev {
+
+Triangle::Triangle(double base, double height, const point_t& centroid)
+  : base_{base}, height_{height}, centroid_{centroid}
+{}
+
+double Triangle::getArea() const
+{
+  return base_ * height_ / 2.0;
+}
+
+rectangle_t Triangle::getFrameRect() const
+{
+  rectangle_t rect;
+  rect.width_ = base_;
+  rect.height_ = height_;
+  rect.pos_ = centroid_;
+  return rect;
+}
+
+}
