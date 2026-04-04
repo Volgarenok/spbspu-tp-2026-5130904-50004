53d6f8284e51f10e4dc1810d4fd2167577117e1c
Fernando Diaz <fernandodiasfd780518@gmail.com>
2026-04-04 15:46:52 -0300

    overallFrame function

--- alberto.fernando/T4/main.cpp
+++ alberto.fernando/T4/main.cpp
@@ -46,3 +46,24 @@ void printShapeInfo(const std::shared_ptr<Shape>& s, const std::string& name)
             << " h=" << fr.height
             << "]\n";
 }
+
+rectangle_t overallFrame(const std::vector<std::shared_ptr<Shape>>& shapes)
+{
+  double minX =  1e18, minY =  1e18;
+  double maxX = -1e18, maxY = -1e18;
+  for (const auto& s : shapes) {
+    rectangle_t fr = s->getFrameRect();
+    double left   = fr.pos.x - fr.width  / 2.0;
+    double right  = fr.pos.x + fr.width  / 2.0;
+    double bottom = fr.pos.y - fr.height / 2.0;
+    double top    = fr.pos.y + fr.height / 2.0;
+    if (left   < minX) minX = left;
+    if (right  > maxX) maxX = right;
+    if (bottom < minY) minY = bottom;
+    if (top    > maxY) maxY = top;
+  }
+  double w = maxX - minX;
+  double h = maxY - minY;
+  point_t c = { (minX + maxX) / 2.0, (minY + maxY) / 2.0 };
+  return { w, h, c };
+}
