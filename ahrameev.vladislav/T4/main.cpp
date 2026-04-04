46662c391a2485962e09246575b7224eedbc030a
EIKO7 <camper1773@mail.ru>
2026-04-04 23:41:17 +0300

    ver 6

--- ahrameev.vladislav/T4/main.cpp
+++ ahrameev.vladislav/T4/main.cpp
@@ -1,5 +1,26 @@
-#include <iostream>
+#include "shape.hpp"
+#include "rectangle.hpp"
+#include "ellipse.hpp"
+#include "rubber.hpp"
+#include <vector>
+#include <memory>
 
-int main()
-{
+namespace akhrameev {
+
+void printAll(const std::vector<std::shared_ptr<Shape>>& storage) {
+    (void)storage;
+}
+
+} 
+
+int main() {
+    using namespace akhrameev;
+
+    std::vector<std::shared_ptr<Shape>> shapes;
+    shapes.emplace_back(std::make_shared<Rectangle>(10.0, 5.0, point_t{0.0, 0.0}));
+    shapes.emplace_back(std::make_shared<Ellipse>(4.0, 6.0, point_t{15.0, 0.0}));
+    shapes.emplace_back(std::make_shared<Rubber>(
+        point_t{-5.0, -5.0}, 2.0, point_t{-2.0, -2.0}, 6.0));
+
+    return 0;
 }
