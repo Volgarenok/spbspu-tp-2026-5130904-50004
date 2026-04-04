399f95718fae8ea292f3613932f6a3bb57415823
EIKO7 <camper1773@mail.ru>
2026-04-04 23:42:47 +0300

    ver 7

--- ahrameev.vladislav/T4/main.cpp
+++ ahrameev.vladislav/T4/main.cpp
@@ -2,13 +2,30 @@
 #include "rectangle.hpp"
 #include "ellipse.hpp"
 #include "rubber.hpp"
+#include <iostream>
 #include <vector>
 #include <memory>
+#include <string>
 
 namespace akhrameev {
 
+void printShapeInfo(const std::string& name, const Shape& s) {
+    rectangle_t r = s.getFrameRect();
+    std::cout << name << ":\n"
+              << "  Area: " << s.getArea() << "\n"
+              << "  FrameRect: w=" << r.width << ", h=" << r.height
+              << ", center=(" << r.pos.x << ", " << r.pos.y << ")\n";
+}
+
 void printAll(const std::vector<std::shared_ptr<Shape>>& storage) {
-    (void)storage;
+    std::vector<std::weak_ptr<Shape>> weak_refs;
+    for (const auto& sp : storage) weak_refs.push_back(sp);
+
+    for (size_t i = 0; i < storage.size(); ++i) {
+        if (auto sp = weak_refs[i].lock()) {
+            printShapeInfo("Shape " + std::to_string(i + 1), *sp);
+        }
+    }
 }
 
 } 
@@ -21,6 +38,9 @@ int main() {
     shapes.emplace_back(std::make_shared<Ellipse>(4.0, 6.0, point_t{15.0, 0.0}));
     shapes.emplace_back(std::make_shared<Rubber>(
         point_t{-5.0, -5.0}, 2.0, point_t{-2.0, -2.0}, 6.0));
+
+    std::cout << "BEFORE SCALING\n";
+    printAll(shapes);
 
     return 0;
 }
