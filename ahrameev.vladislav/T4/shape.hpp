d73edc60f04c187b60f83544c24cf09c65596bfb
EIKO7 <camper1773@mail.ru>
2026-04-04 23:29:29 +0300

    ver 2 abstract Shape base class

--- ahrameev.vladislav/T4/shape.hpp
+++ ahrameev.vladislav/T4/shape.hpp
@@ -14,6 +14,16 @@ struct rectangle_t {
     point_t pos{0.0, 0.0};
 };
 
+class Shape {
+public:
+    virtual ~Shape() = default;
+    virtual double getArea() const = 0;
+    virtual rectangle_t getFrameRect() const = 0;
+    virtual void move(point_t to) = 0;
+    virtual void move(double dx, double dy) = 0;
+    virtual void scale(double factor) = 0;
+};
+
 } 
 
 #endif 
