3f893d4da95264af8f7aa52ff5af82f45b09fc3c
ryouta25 <avp22.07@mail.ru>
2026-04-04 21:01:29 +0300

    add Rectangle move and scale methods

--- pozdeev.andrey/T4/rectangle.cpp
+++ pozdeev.andrey/T4/rectangle.cpp
@@ -16,4 +16,21 @@ rectangle_t Rectangle::getFrameRect() const
   return rect_;
 }
 
+void Rectangle::move(double dx, double dy)
+{
+  rect_.pos_.x_ += dx;
+  rect_.pos_.y_ += dy;
+}
+
+void Rectangle::move(const point_t& point)
+{
+  rect_.pos_ = point;
+}
+
+void Rectangle::scale(double coefficient)
+{
+  rect_.width_ *= coefficient;
+  rect_.height_ *= coefficient;
+}
+
 }
