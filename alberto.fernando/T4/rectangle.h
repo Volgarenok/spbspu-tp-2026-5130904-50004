0a4d248b2ddca2ec183755c38f69201ac00e6b56
Fernando Diaz <fernandodiasfd780518@gmail.com>
2026-04-04 14:49:08 -0300

    Rectangle Class Declaration

---
+++ alberto.fernando/T4/rectangle.h
@@ -0,0 +1,27 @@
+#ifndef RECTANGLE_HPP
+#define RECTANGLE_HPP
+
+#include "shape.hpp"
+
+namespace alberto {
+
+class Rectangle : public Shape {
+public:
+  Rectangle(const point_t& center, double width, double height);
+  Rectangle(const point_t& bottomLeft, const point_t& topRight);
+
+  double getArea() const override;
+  rectangle_t getFrameRect() const override;
+  void move(const point_t& dest) override;
+  void move(double dx, double dy) override;
+  void scale(double factor) override;
+
+private:
+  point_t center_;
+  double width_;
+  double height_;
+};
+
+}
+
+#endif
