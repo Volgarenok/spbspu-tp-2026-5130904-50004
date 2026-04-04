c5d3a201ec6bb5241e64c772c7f2a4d9106c81ed
ryouta25 <avp22.07@mail.ru>
2026-04-04 21:30:11 +0300

    add functions in main.cpp

--- pozdeev.andrey/T4/main.cpp
+++ pozdeev.andrey/T4/main.cpp
@@ -1,2 +1,25 @@
+#include <iostream>
+#include <memory>
+#include <vector>
+
+#include "shape.hpp"
+#include "rectangle.hpp"
+#include "diamond.hpp"
+#include "triangle.hpp"
+#include "utils.hpp"
+
 int main()
-{}
+{
+  std::vector<std::shared_ptr<pozdeev::Shape>> shapes;
+  shapes.emplace_back(std::make_shared<pozdeev::Rectangle>(10.0, 5.0, pozdeev::point_t(0.0, 0.0)));
+  shapes.emplace_back(std::make_shared<pozdeev::Diamond>(8.0, 6.0, pozdeev::point_t(5.0, 5.0)));
+  shapes.emplace_back(std::make_shared<pozdeev::Triangle>(12.0, 9.0, pozdeev::point_t(-3.0, 4.0)));
+
+  std::cout << "Initial state:\n";
+  pozdeev::printShapesInfo(shapes);
+
+  std::cout << "Enter scale point (x y) and coefficient: ";
+  double pointX = 0.0;
+  double pointY = 0.0;
+  double scaleFactor = 0.0;
+}
