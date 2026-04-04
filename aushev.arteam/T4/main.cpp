d1054656237298fdf4d5c34bd88006c424094651
Ausev.Arteam <seregabest77@gmail.com>
2026-04-04 19:11:43 +0300

    main

--- aushev.arteam/T4/main.cpp
+++ aushev.arteam/T4/main.cpp
@@ -1,5 +1,54 @@
+#include "shape.hpp"
+#include "rectangle.hpp"
+#include "triangle.hpp"
+#include "diamond.hpp"
+#include "functions.hpp"
+
+#include <string>
+#include <vector>
 #include <iostream>
 
-int main(){
-    return 0;
+using namespace aushev;
+
+int main()
+{
+  std::vector<std::shared_ptr<Shape>> shps;
+  std::vector<std::string> nms;
+
+  try
+  {
+    shps.push_back(std::make_shared<Rectangle>(7.0, 2.8, point_t(1.0, 2.0)));
+    nms.push_back("Rectangle");
+
+    shps.push_back(std::make_shared<Triangle>(point_t(0.0, 0.0), point_t(4.0, 0.0), point_t(2.0, 3.0)));
+    nms.push_back("Triangle");
+
+    shps.push_back(std::make_shared<Diamond>(6.0, 4.0, point_t(5.0, 5.0)));
+    nms.push_back("Diamond");
+  }
+  catch (const std::exception& err)
+  {
+    std::cerr << "Failed to create figures: " << err.what() << "\n";
+    return 1;
+  }
+
+  std::vector<std::weak_ptr<Shape>> wshps;
+  for (const auto& sh : shps)
+  {
+    wshps.push_back(sh);
+  }
+
+  std::cout << "BEFORE:\n\n";
+  try
+  {
+    displayAll(wshps, nms);
+    displayTotalFrame(getTotalFrame(wshps));
+  }
+  catch (const std::exception& err)
+  {
+    std::cerr << "Failed to display figures: " << err.what() << "\n";
+    return 1;
+  }
+
+  return 0;
 }
