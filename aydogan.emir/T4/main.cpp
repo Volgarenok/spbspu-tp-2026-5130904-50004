a1aba936921a2de74b1bb4dab6458479070160b7
emiraydgn <emiraydogan03@gmail.com>
2026-04-28 01:23:28 +0300

    main skeleton

---
+++ aydogan.emir/T4/main.cpp
@@ -0,0 +1,13 @@
+#include "rectangle.hpp"
+#include "square.hpp"
+#include "triangle.hpp"
+#include "functions.hpp"
+
+#include <iostream>
+#include <memory>
+#include <vector>
+
+int main()
+{
+  return 0;
+}
