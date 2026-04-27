9bd32fe2d6def17e0858b7011c89df80617a3730
emiraydgn <emiraydogan03@gmail.com>
2026-04-27 23:46:17 +0300

    Rectangle class members

--- aydogan.emir/T4/rectangle.hpp
+++ aydogan.emir/T4/rectangle.hpp
@@ -8,9 +8,18 @@ namespace aydogan
   class Rectangle: public Shape
   {
   public:
+    Rectangle(double width, double height, const point_t& center);
+
+    double getArea() const override;
+    rectangle_t getFrameRect() const override;
+    void move(const point_t& point) override;
+    void move(double dx, double dy) override;
+    void scale(double coefficient) override;
 
   private:
-
+    double width_;
+    double height_;
+    point_t center_;
   };
 }
 
