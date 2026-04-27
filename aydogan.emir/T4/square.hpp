835deaff204dcf818d2593d6ee6f8342c00a671a
emiraydgn <emiraydogan03@gmail.com>
2026-04-28 00:21:09 +0300

    Square interface and data members

--- aydogan.emir/T4/square.hpp
+++ aydogan.emir/T4/square.hpp
@@ -8,9 +8,17 @@ namespace aydogan
   class Square: public Shape
   {
   public:
+    Square(double side, const point_t& center);
+
+    double getArea() const override;
+    rectangle_t getFrameRect() const override;
+    void move(const point_t& point) override;
+    void move(double dx, double dy) override;
+    void scale(double coefficient) override;
 
   private:
-
+    double side_;
+    point_t center_;
   };
 }
 
