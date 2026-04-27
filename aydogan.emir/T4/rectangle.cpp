19b5d8177117b6d23f2bca3c62914f850d843350
emiraydgn <emiraydogan03@gmail.com>
2026-04-27 23:53:59 +0300

    Rectangle scaling and move functions

--- aydogan.emir/T4/rectangle.cpp
+++ aydogan.emir/T4/rectangle.cpp
@@ -16,3 +16,30 @@ double aydogan::Rectangle::getArea() const
 {
   return width_ * height_;
 }
+
+aydogan::rectangle_t aydogan::Rectangle::getFrameRect() const
+{
+  return rectangle_t(width_, height_, center_);
+}
+
+void aydogan::Rectangle::move(const point_t& point)
+{
+  center_ = point;
+}
+
+void aydogan::Rectangle::move(double dx, double dy)
+{
+  center_.x_ += dx;
+  center_.y_ += dy;
+}
+
+void aydogan::Rectangle::scale(double coefficient)
+{
+  if (coefficient <= 0.0)
+  {
+    throw std::invalid_argument("Invalid scale coefficient");
+  }
+
+  width_ *= coefficient;
+  height_ *= coefficient;
+}
