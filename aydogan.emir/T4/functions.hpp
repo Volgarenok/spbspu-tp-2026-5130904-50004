f0680c30caf1dc24da3bc5e4328c72905a77f87f
emiraydgn <emiraydogan03@gmail.com>
2026-04-28 01:04:10 +0300

    functions skeleton

---
+++ aydogan.emir/T4/functions.hpp
@@ -0,0 +1,14 @@
+#ifndef FUNCTIONS_HPP
+#define FUNCTIONS_HPP
+
+#include "shape.hpp"
+#include <iosfwd>
+#include <string>
+#include <vector>
+
+namespace aydogan
+{
+  using ShapeArray = std::vector< ShapePtr >;
+}
+
+#endif
