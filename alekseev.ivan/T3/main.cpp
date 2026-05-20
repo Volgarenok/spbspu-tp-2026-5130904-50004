d6238098d64ffaf8e2019aeb8abb2cf8a06783c6
alekseev-ii <alekseev.ii@edu.spbstu.ru>
2026-05-20 21:44:44 +0300

    feat(main): add base commands declaration

--- alekseev.ivan/T3/main.cpp
+++ alekseev.ivan/T3/main.cpp
@@ -1,3 +1,18 @@
+#include <functional>
+#include "Polygon.h"
+
+namespace alekseev {
+  using data_t = std::vector< Polygon >;
+  using args_t = std::vector< std::string >;
+  using command = std::function< double (data_t &, const args_t &) >;
+  using const_command = std::function< double (const data_t &, const args_t &) >;
+
+  double area(const data_t &, const args_t &);
+  double max(const data_t &, const args_t &);
+  double min(const data_t &, const args_t &);
+  double count(const data_t &, const args_t &);
+}
+
 int main()
 {
 }
