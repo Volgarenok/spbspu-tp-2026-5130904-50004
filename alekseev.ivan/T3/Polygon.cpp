faac2fcba302d5d07fab3cac5f50b5927c824e33
alekseev-ii <alekseev.ii@edu.spbstu.ru>
2026-05-20 21:18:30 +0300

    feat(Polygon): copy expected and IOGuard from T2

--- alekseev.ivan/T3/Polygon.cpp
+++ alekseev.ivan/T3/Polygon.cpp
@@ -3,3 +3,36 @@
 //
 
 #include "Polygon.h"
+
+std::istream & alekseev::operator>>(std::istream & is, expected e)
+{
+  if (!is) {
+    return is;
+  }
+  char c = 0;
+  for (size_t i = 0; i < e.source.length(); ++i) {
+    is >> c;
+    if (c != e.source[i]) {
+      is.setstate(std::ios_base::failbit);
+      return is;
+    }
+  }
+  return is;
+}
+
+alekseev::IOGuard::IOGuard(std::basic_ios< char > & stream):
+  stream_(stream),
+  precision_(stream.precision()),
+  width_(stream.width()),
+  flags_(stream.flags()),
+  fill_(stream.fill())
+{
+}
+
+alekseev::IOGuard::~IOGuard()
+{
+  stream_.precision(precision_);
+  stream_.width(width_);
+  stream_.flags(flags_);
+  stream_.fill(fill_);
+}
--- alekseev.ivan/T3/Polygon.h
+++ alekseev.ivan/T3/Polygon.h
@@ -18,6 +18,24 @@ namespace alekseev {
 
   std::istream & operator>>(std::istream & is, Polygon & p);
   std::ostream & operator<<(std::ostream & os, const Polygon & p);
+
+  struct expected {
+    std::string source;
+  };
+
+  std::istream & operator>>(std::istream & is, expected e);
+
+  struct IOGuard {
+    explicit IOGuard(std::basic_ios< char > & stream);
+    ~IOGuard();
+
+    private:
+      std::basic_ios< char > & stream_;
+      std::streamsize precision_;
+      std::streamsize width_;
+      std::basic_ios< char >::fmtflags flags_;
+      char fill_;
+  };
 }
 
 #endif
