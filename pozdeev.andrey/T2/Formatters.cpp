7ee3bc5ad26b0e3856c5ee9053b8a9e2763ee4c2
ryouta25 <avp22.07@mail.ru>
2026-06-07 00:17:50 +0300

    реализация форматтеров для разделителей и строк

--- pozdeev.andrey/T2/Formatters.cpp
+++ pozdeev.andrey/T2/Formatters.cpp
@@ -17,4 +17,40 @@ namespace pozdeev
     s_.precision(precision_);
     s_.fill(fill_);
   }
+
+  std::istream& operator>>(std::istream& in, DelimiterIO&& dest)
+  {
+    std::istream::sentry sentry(in);
+    if (!sentry)
+    {
+      return in;
+    }
+    char c = '0';
+    in >> c;
+    if (in && (c != dest.exp))
+    {
+      in.setstate(std::ios::failbit);
+    }
+    return in;
+  }
+
+  std::istream& operator>>(std::istream& in, StringIO&& dest)
+  {
+    std::istream::sentry sentry(in);
+    if (!sentry)
+    {
+      return in;
+    }
+    return in >> std::quoted(dest.ref);
+  }
+
+  std::ostream& operator<<(std::ostream& out, const StringOut& dest)
+  {
+    std::ostream::sentry sentry(out);
+    if (!sentry)
+    {
+      return out;
+    }
+    return out << std::quoted(dest.val);
+  }
 }
