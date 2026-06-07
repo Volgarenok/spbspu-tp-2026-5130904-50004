efd62d755ef0b0b50c2602cc6f6a6ce84b8aba59
ryouta25 <avp22.07@mail.ru>
2026-06-07 10:19:20 +0300

    реализация вычисления площади и предикат вершин

--- pozdeev.andrey/T3/Geometry.cpp
+++ pozdeev.andrey/T3/Geometry.cpp
@@ -2,6 +2,8 @@
 #include "Formatters.hpp"
 #include <iterator>
 #include <algorithm>
+#include <numeric>
+#include <cmath>
 
 namespace pozdeev
 {
@@ -45,5 +47,49 @@ namespace pozdeev
   bool operator==(const Point& lhs, const Point& rhs)
   {
     return lhs.x == rhs.x && lhs.y == rhs.y;
+  }
+
+  double crossProduct(const Point& a, const Point& b)
+  {
+    return static_cast< double >(a.x) * b.y - static_cast< double >(a.y) * b.x;
+  }
+
+  double getArea(const Polygon& p)
+  {
+    std::vector< double > products(p.points.size());
+    std::transform(p.points.begin(), p.points.end() - 1, p.points.begin() + 1, products.begin(), crossProduct);
+    products.back() = crossProduct(p.points.back(), p.points.front());
+    double sum = std::accumulate(products.begin(), products.end(), 0.0);
+    return std::abs(sum) / 2.0;
+  }
+
+  double getAreaIfEven(const Polygon& p)
+  {
+    return isEven(p) ? getArea(p) : 0.0;
+  }
+
+  double getAreaIfOdd(const Polygon& p)
+  {
+    return isOdd(p) ? getArea(p) : 0.0;
+  }
+
+  double getAreaIfNum(const Polygon& p, size_t vertexes)
+  {
+    return hasVertexes(p, vertexes) ? getArea(p) : 0.0;
+  }
+
+  bool isEven(const Polygon& p)
+  {
+    return p.points.size() % 2 == 0;
+  }
+
+  bool isOdd(const Polygon& p)
+  {
+    return p.points.size() % 2 != 0;
+  }
+
+  bool hasVertexes(const Polygon& p, size_t vertexes)
+  {
+    return p.points.size() == vertexes;
   }
 }
