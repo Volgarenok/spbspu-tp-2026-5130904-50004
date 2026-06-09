a510ed83381dbc0440afa8358d92b7550f141d94
Ausev.Arteam <seregabest77@gmail.com>
2026-06-09 21:03:07 +0300

    Добавление логики парсинга данных

--- aushev.arteam/T3/geometry.hpp
+++ aushev.arteam/T3/geometry.hpp
@@ -4,6 +4,8 @@
 #include <vector>
 #include <cmath>
 #include <algorithm>
+#include <iostream>
+#include <string>
 
 namespace aushev {
 
@@ -36,6 +38,79 @@ struct Polygon {
         return std::abs(area) / 2.0;
     }
 };
+
+inline std::istream& operator>>(std::istream& is, Polygon& poly) {
+    std::string line;
+    while (std::getline(is, line)) {
+        size_t pos = 0;
+        while (pos < line.size() && std::isspace(line[pos])) {
+            ++pos;
+        }
+        if (pos == line.size()) {
+            continue;
+        }
+
+        size_t n;
+        size_t start = pos;
+        while (pos < line.size() && !std::isspace(line[pos])) {
+            ++pos;
+        }
+        try {
+            n = std::stoull(line.substr(start, pos - start));
+        } catch (...) {
+            continue;
+        }
+
+        std::vector<Point> pts;
+        bool valid = true;
+        for (size_t i = 0; i < n; ++i) {
+            while (pos < line.size() && std::isspace(line[pos])) {
+                ++pos;
+            }
+            if (pos >= line.size() || line[pos] != '(') {
+                valid = false;
+                break;
+            }
+            ++pos;
+            size_t p1 = pos;
+            while (pos < line.size() && line[pos] != ';') {
+                ++pos;
+            }
+            if (pos >= line.size()) {
+                valid = false;
+                break;
+            }
+            int x = std::stoi(line.substr(p1, pos - p1));
+            ++pos;
+            size_t p2 = pos;
+            while (pos < line.size() && line[pos] != ')') {
+                ++pos;
+            }
+            if (pos >= line.size()) {
+                valid = false;
+                break;
+            }
+            int y = std::stoi(line.substr(p2, pos - p2));
+            ++pos;
+            pts.push_back({x, y});
+        }
+        if (valid && pts.size() == n) {
+            poly.points_ = std::move(pts);
+            return is;
+        }
+    }
+    is.setstate(std::ios::eofbit);
+    return is;
+}
+
+inline std::ostream& operator<<(std::ostream& os, const Polygon& poly) {
+    os << poly.points_.size();
+    for (const auto& p : poly.points_) {
+        os << " (" << p.x_ << ";" << p.y_ << ")";
+    }
+    os << "\n";
+    return os;
+}
 
 }
 
