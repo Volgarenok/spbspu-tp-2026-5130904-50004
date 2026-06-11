f9b5348ac5bd852c22d217fa95f1f7371e74c5dc
GenJune <julia.nepochatova@gmail.com>
2026-06-11 21:19:29 +0300

    Keeping IOGuard struct and Delimiter from T2

---
+++ nepochatova.yulia/T3/IOGuard .cpp
@@ -0,0 +1,31 @@
+#include "IOGuard.h"
+#include <cctype>
+
+namespace nepochatova {
+  IOguard::IOguard(std::basic_ios<char>& s):
+    s_(s),
+    precision_(s.precision()),
+    width_(s.width()),
+    fmt_(s.flags()),
+    fill_(s.fill())
+  {}
+
+  IOguard::~IOguard() {
+    s_.precision(precision_);
+    s_.width(width_);
+    s_.flags(fmt_);
+    s_.fill(fill_);
+  }
+
+  std::istream& operator>>(std::istream& in, DelimiterIO&& dest)
+  {
+    std::istream::sentry s(in);
+    if (!s) return in;
+    in >> dest.last_;
+    if (in && (std::tolower(static_cast<unsigned char>(dest.last_)) !=
+               std::tolower(static_cast<unsigned char>(dest.expected_)))) {
+      in.setstate(std::ios_base::failbit);
+               }
+    return in;
+  }
+}
---
+++ nepochatova.yulia/T3/IOGuard.h
@@ -0,0 +1,31 @@
+#ifndef IOGUARD_H
+#define IOGUARD_H
+
+#include <ios>
+#include <string>
+#include <istream>
+#include <ostream>
+
+namespace nepochatova {
+  class IOguard
+  {
+  public:
+    explicit IOguard(std::basic_ios<char>& s);
+    ~IOguard();
+  private:
+    std::basic_ios<char>& s_;
+    std::streamsize precision_;
+    std::streamsize width_;
+    std::basic_ios<char>::fmtflags fmt_;
+    char fill_;
+  };
+
+  struct DelimiterIO {
+    char expected_;
+    char& last_;
+  };
+  std::istream& operator>>(std::istream& in, DelimiterIO&& dest);
+
+}
+
+#endif
