cb496265e8da594b52d457304ff0b4056f7cc0c9
GenJune <julia.nepochatova@gmail.com>
2026-06-12 11:55:18 +0300

    geometry shapes structs skeleton

---
+++ nepochatova.yulia/T3/Geometry.h
@@ -0,0 +1,29 @@
+#ifndef GEOMETRY_H
+#define GEOMETRY_H
+
+#include <vector>
+#include <iostream>
+
+
+namespace nepochatova {
+  struct Point {
+    int x, y;
+
+    bool operator==(const Point &other) const;
+  };
+
+  struct Polygon {
+    std::vector<Point> points;
+
+    bool operator==(const Polygon &other) const;
+  };
+
+  struct PointIO {
+    Point &ref;
+  };
+
+  std::istream &operator>>(std::istream &in, PointIO &&dest);
+  std::istream &operator>>(std::istream &in, Polygon &poly);
+
+}
+#endif
