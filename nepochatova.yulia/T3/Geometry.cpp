325e2f0e0a4736b529eb5d4333c3a360019b2bc8
GenJune <julia.nepochatova@gmail.com>
2026-06-12 12:55:43 +0300

    operator >> realisation

--- nepochatova.yulia/T3/Geometry.cpp
+++ nepochatova.yulia/T3/Geometry.cpp
@@ -16,4 +16,40 @@ namespace nepochatova {
       in.setstate(std::ios_base::failbit);
     return in;
   }
+
+  std::istream& operator>>(std::istream& in, PointIO&& dest) {
+    std::istream::sentry s(in);
+    if (!s) return in;
+
+    char dummy = 0;
+
+    in >> DelimiterIO{'(', dummy}
+    >> dest.ref.x
+    >> SemiDelim{';'}
+    >> dest.ref.y
+    >> DelimiterIO{')', dummy};
+
+    return in;
+  }
+
+  std::istream& operator>>(std::istream& in, Polygon& poly) {
+    std::istream::sentry s(in);
+    if (!s) return in;
+
+    size_t n;
+    if (!(in >> n)) return in;
+
+    poly.points.clear();
+    poly.points.reserve(n);
+
+    for (size_t i = 0; i < n; ++i) {
+      Point p;
+      if (!(in >> PointIO{p})) {
+        in.setstate(std::ios_base::failbit);
+        return in;
+      }
+      poly.points.push_back(p);
+    }
+    return in;
+  }
 }
