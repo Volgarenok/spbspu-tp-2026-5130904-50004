63b79c0731a53f99e6228ed2c63e27066551603a
Farida <velizadefarida@gmail.com>
2026-06-20 15:06:23 +0300

    add shape.hpp

---
+++ velizade.farida/T4/shape.hpp
@@ -0,0 +1,21 @@
+#ifndef SHAPE_HPP
+#define SHAPE_HPP
+
+#include "rectangle.hpp"
+
+namespace velizade
+{
+  class Shape
+  {
+  public:
+    virtual ~Shape() = default;
+
+    virtual double getArea() const = 0;
+    virtual rectangle_t getFrameRect() const = 0;
+    virtual void move(const point_t& newPos) = 0;
+    virtual void move(double dx, double dy) = 0;
+    virtual void scale(double k) = 0;
+  };
+}
+
+#endif
