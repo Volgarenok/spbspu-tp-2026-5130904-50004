d5341df02e92dbed0e06e40afdb0146051f79fc5
Emich-M <fagetolrer@gmail.com>
2026-06-24 18:35:23 +0300

    Implement file reading

--- em.maksim/T3/main.cpp
+++ em.maksim/T3/main.cpp
@@ -1,5 +1,26 @@
 #include "polygon.hpp"
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
 
-int main() {
+int main(int argc, char* argv[]) {
+  if (argc != 2) {
+    std::cerr << "Usage: " << argv[0] << " <filename>\n";
+    return 1;
+  }
+
+  std::ifstream file(argv[1]);
+  if (!file) {
+    std::cerr << "Cannot open file: " << argv[1] << "\n";
+    return 1;
+  }
+
+  std::vector<em::Polygon> shapes;
+  em::Polygon poly;
+  while (file >> poly) {
+    shapes.push_back(poly);
+  }
+
   return 0;
 }
