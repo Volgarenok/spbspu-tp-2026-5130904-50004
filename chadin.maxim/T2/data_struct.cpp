cee49ed1077a78ebead71745b103e8e15def4c38
maxchadin <maxchadin080418@gmail.com>
2026-06-25 03:30:41 +0300

    add DataStruct& rhs in data_struct.cpp

--- chadin.maxim/T2/data_struct.cpp
+++ chadin.maxim/T2/data_struct.cpp
@@ -10,3 +10,13 @@ namespace chadin {
     isValid(false)
   {
   }
+
+  bool operator<(const DataStruct& lhs, const DataStruct& rhs)
+  {
+    if (lhs.key1 != rhs.key1) {
+      return lhs.key1 < rhs.key1;
+    } else if (lhs.key2 != rhs.key2) {
+      return lhs.key2 < rhs.key2;
+    }
+    return lhs.key3.length() < rhs.key3.length();
+  }
