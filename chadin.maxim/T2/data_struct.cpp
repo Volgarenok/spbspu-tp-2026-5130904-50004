f61fb5aca983aee6971d47b0a9c00ba3afd24629
maxchadin <maxchadin080418@gmail.com>
2026-06-25 03:28:50 +0300

    create data_struct.cpp

---
+++ chadin.maxim/T2/data_struct.cpp
@@ -0,0 +1,12 @@
+#include "data_struct.hpp"
+#include "io_formats.hpp"
+#include <iomanip>
+
+namespace chadin {
+  DataStruct::DataStruct():
+    key1(0.0),
+    key2(0),
+    key3(""),
+    isValid(false)
+  {
+  }
