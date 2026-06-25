4e463c8594e36707a38abe46c31a4bd2d6a8c0fe
maxchadin <maxchadin080418@gmail.com>
2026-06-25 03:39:56 +0300

    add DelimiterIO to io_formats.cpp

--- chadin.maxim/T2/io_formats.cpp
+++ chadin.maxim/T2/io_formats.cpp
@@ -15,3 +15,17 @@ namespace chadin {
     stream_.precision(precision_);
     stream_.flags(flags_);
   }
+
+  std::istream& operator>>(std::istream& in, DelimiterIO&& dest)
+  {
+    std::istream::sentry sentry(in);
+    if (!sentry) {
+      return in;
+    }
+    char current = '0';
+    in >> current;
+    if (in && current != dest.expected) {
+      in.setstate(std::ios::failbit);
+    }
+    return in;
+  }
