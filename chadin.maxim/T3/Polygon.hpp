80b6c3fbb8059ff55c80f116167f9c8be9daf7ea
maxchadin <maxchadin080418@gmail.com>
2026-06-25 22:52:47 +0300

    added Polygon struct to Polygon.hpp

--- chadin.maxim/T3/Polygon.hpp
+++ chadin.maxim/T3/Polygon.hpp
@@ -13,3 +13,15 @@ namespace chadin {
 
   bool operator==(const Point& lhs, const Point& rhs);
   std::istream& operator>>(std::istream& in, Point& point);
+
+  struct Polygon
+  {
+    std::vector<Point> points;
+  };
+
+  bool operator==(const Polygon& lhs, const Polygon& rhs);
+  std::istream& operator>>(std::istream& in, Polygon& polygon);
+
+}
+
+#endif
