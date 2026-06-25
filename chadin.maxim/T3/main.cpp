6aa899feb1dcec6789e43e9f53b7ebaed4a1ea72
maxchadin <maxchadin080418@gmail.com>
2026-06-25 23:24:04 +0300

    added LineReader struct and parse_polygon struct to main.cpp

--- chadin.maxim/T3/main.cpp
+++ chadin.maxim/T3/main.cpp
@@ -1,2 +1,35 @@
-int main()
-{}
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <iterator>
+#include <string>
+#include <algorithm>
+#include <sstream>
+#include "Polygon.hpp"
+#include "Commands.hpp"
+
+namespace chadin {
+  namespace detail {
+
+    struct LineReader
+    {
+      std::string line;
+      operator std::string() const { return line; }
+      friend std::istream& operator>>(std::istream& in, LineReader& lr)
+      {
+        return std::getline(in, lr.line);
+      }
+    };
+
+    struct parse_polygon
+    {
+      Polygon operator()(const std::string& str) const
+      {
+        std::istringstream iss(str);
+        Polygon p;
+        if (iss >> p) {
+          return p;
+        }
+        return {};
+      }
+    };
