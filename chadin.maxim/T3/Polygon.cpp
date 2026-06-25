7b59ff8be17c1f1b5a00448141a6350ff6566a2f
maxchadin <maxchadin080418@gmail.com>
2026-06-25 22:56:25 +0300

    update Polygon.cpp

--- chadin.maxim/T3/Polygon.cpp
+++ chadin.maxim/T3/Polygon.cpp
@@ -17,3 +17,30 @@ namespace chadin {
     };
 
   }
+
+  bool operator==(const Point& lhs, const Point& rhs)
+  {
+    return lhs.x == rhs.x && lhs.y == rhs.y;
+  }
+
+  std::istream& operator>>(std::istream& in, Point& point)
+  {
+    std::istream::sentry sentry(in);
+    if (!sentry) {
+      return in;
+    }
+    char c1 = '\0';
+    char c2 = '\0';
+    char c3 = '\0';
+    int x = 0;
+    int y = 0;
+    if (in >> c1 >> x >> c2 >> y >> c3) {
+      if (c1 == '(' && c2 == ';' && c3 == ')') {
+        point.x = x;
+        point.y = y;
+      } else {
+        in.setstate(std::ios::failbit);
+      }
+    }
+    return in;
+  }
